10e026219a65ab904d2eda7705d86d5ddd7dd668
johnpfay <john.fay@duke.edu>
2015-07-06 09:31:45 -0400

    Finalized R_ScreenCor script. Integrated with ArcGIS

--- Scripts/R_ScreenCor.py
+++ Scripts/R_ScreenCor.py
@@ -16,16 +16,16 @@ import sys, os, csv, arcpy, numpy
 arcpy.env.overwriteOutput = 1
 
 '''DEBUG INPUTS
-C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\RData\E_complanata2.csv
+C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\RData\E_complanata2.csv C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\RData\E_complanata2_shcX.csv 0.7 C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\RData\E_complanata2_coeff.csv
 '''
 
 # Input variables
-speciesCSV = r'C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\RData\E_complanata2.csv'#arcpy.GetParameterAsText(0)
-fieldsCSV = r'C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\RData\E_complanata2_shcX.csv'#arcpy.GetParameterAsText(1)
-threshold = "0.7"
+speciesCSV = arcpy.GetParameterAsText(0)
+fieldsCSV = arcpy.GetParameterAsText(1)
+threshold = arcpy.GetParameterAsText(2)
 
 # Output variables
-coeffCSV = r'C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\RData\E_complanata2_coeff.csv'
+coeffCSV = arcpy.GetParameterAsText(3)
 
 # Script variables
 
@@ -49,11 +49,12 @@ line = f.readline()         # Get the first data line
 while line:                 # Loop through the remaining lines
     lineData = f.readline().split(",")  # Get the string in the line and split it into a list
     if len(lineData) > 1:               # If the line includes field names (the last one may not)
-        colName = lineData[1]                      # Get the column name (second item)
+        colName = lineData[0]                      # Get the column name (first item)
         colName = colName.replace('"','')          # Remove any quotes
         colNames.append(colName)                   # Add the column name to the list 
     line = f.readline()                 # Go to the next line in the CSV file
 f.close()                   # Close the file
+#colNames.remove("TEMP0001")
 msg("{} columns to analyze".format(len(colNames)))
 
 #Read in all the column header names (from the full data file)
@@ -76,22 +77,23 @@ f.write("Var1, Var2, Coeff\n")
 
 #Loop through column pairs
 msg("Computing correlation values")
-for i in range(1,nCols):
-    for j in range(i+1,nCols):
-        # Get the names of the two columns
-        name1 = headerItems[i]
-        name2 = headerItems[j]
-        # Process only if both the columns are in the colNames list (and the pairs aren't the same)
-        if name1 in colNames and name2 in colNames and not (name1 == name2):
-            # Get the data vectors of the two columns
-            v1 = arrData[1:,i]
-            v2 = arrData[1:,j]
-            # Calculate the correlation coefficient
-            pearson = numpy.corrcoef(v1,v2)[0,1]
-            # If the coefficient is > the threshold consider them redundant and add to the CSV
-            if abs(pearson) >= float(threshold):
-                f.write("{}, {}, {}\n".format(name1,name2,pearson))
-                #msg("{},{},{}".format(name1,name2,pearson))
+for i in range(0,nCols):
+    for j in range(0,nCols):
+        if j > i:
+            # Get the names of the two columns
+            name1 = headerItems[i]
+            name2 = headerItems[j]
+            # Process only if both the columns are in the colNames list (and the pairs aren't the same)
+            if name1 in colNames and name2 in colNames and not (name1 == name2):
+                # Get the data vectors of the two columns
+                v1 = arrData[1:,i]
+                v2 = arrData[1:,j]
+                # Calculate the correlation coefficient
+                pearson = numpy.corrcoef(v1,v2)[0,1]
+                # If the coefficient is > the threshold consider them redundant and add to the CSV
+                if abs(pearson) >= float(threshold):
+                    f.write("{}, {}, {}\n".format(str(name1),name2,pearson))
+                    #msg("{},{},{}".format(name1,name2,pearson))
 
 msg("Values written to {}".format(coeffCSV))
 f.close()
