107b3e602a1765fd897fb3f64eb4652304658e46
johnpfay <john.fay@duke.edu>
2015-07-13 13:40:21 -0400

    Added ArcGIS interface, streamlined script

--- Scripts/MAXENT_DisplayModelResults.py
+++ Scripts/MAXENT_DisplayModelResults.py
@@ -9,11 +9,11 @@ import arcpy, sys, os, csv
 arcpy.env.overwriteOutput = 1
 
 # Input variables
-CatchmentFC = r"C:\WorkSpace\EEP_Spring2015\EEP_Tool\Data\EEP_030501.gdb\EnvStats"
-maxentFolder = r"C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\Nocomis_leptocephalus\Output"
+CatchmentFC = arcpy.GetParameterAsText(0)  #r"C:\WorkSpace\EEP_Spring2015\EEP_Tool\Data\EEP_030501.gdb\EnvStats"
+maxentFolder = arcpy.GetParameterAsText(1) #r"C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\Nocomis_leptocephalus\Output"
 
 # Output
-outFC = r"C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\Nocomis_leptocephalus\HabModel.shp"
+outFC = arcpy.GetParameterAsText(2) #r"C:\WorkSpace\EEP_Spring2015\EEP_Tool\Scratch\Nocomis_leptocephalus\HabModel.shp"
 
 # ---Functions---
 def msg(txt,type="message"):
@@ -28,47 +28,84 @@ def msg(txt,type="message"):
 # ---Processes---
 ## Get the MaxEnt result files
 #Get the species name from the maxent log file
-f = open(os.path.join(maxentFolder,"maxent.log"))
+logFN = os.path.join(maxentFolder,"maxent.log")
+if not os.path.exists(logFN):
+    msg("File {} does not exist.\nExiting.".format(logFN),"error")
+    sys.exit()
+f = open(logFN,"r")
 lines = f.readlines()
 f.close()
 sppLine = lines[11]
 sppName = sppLine[9:-1]
 
 #Get the result filenames
+msg("Getting Maxent files...")
 resultsFN = os.path.join(maxentFolder,"maxentResults.csv")
 logisticFN = os.path.join(maxentFolder,sppName+".csv")
 predictionFN = os.path.join(maxentFolder,sppName+"_samplePredictions.csv")
+
+#Make sure the results exist
+for f in (resultsFN, logisticFN, predictionFN):
+    if not os.path.exists(f):
+        msg("File {} does not exist.\nExiting.".format(f),"error")
+        sys.exit()
 
 #Get the threshold (column 255 in the maxentResults.csv)
 f = open(resultsFN,'rt')
 reader = csv.reader(f)
 row1 = reader.next()
 row2 = reader.next()
-threshold = row2[254]
 f.close()
+idx = row1.index('Balance training omission, predicted area and threshold value logistic threshold')
+threshold = row2[idx]
+msg("Maxent logistic threshold set to {}".format(threshold))
 
 #Create a table of from the logistic CSV
+msg("Converting {} to a table".format(logisticFN))
 logTbl = arcpy.CopyRows_management(logisticFN,"in_memory/Logistic")
 
 #Get the logistic field name
+msg("Getting list of habitat attributes used in model.")
 logFld = arcpy.ListFields(logTbl)[-1].name
 
-#Copy the Feature class
-arcpy.CopyFeatures_management(CatchmentFC, outFC)
+#Make a feature layer of the catchment features
+msg("Creating a feature layer of catchment features")
+fldInfo = arcpy.FieldInfo()
+for f in arcpy.ListFields(CatchmentFC):
+    fName = f.name
+    if fName in ("OBJECTID","Shape","GRIDCODE","FeatureID"):
+        fldInfo.addField(fName,fName,"VISIBLE","")
+    else:
+        fldInfo.addField(fName,fName,"HIDDEN","")
+catchLyr = arcpy.MakeFeatureLayer_management(CatchmentFC,"catchLyr","","",fldInfo)
 
-#Remove all but the first three fields
-for fld in arcpy.ListFields(outFC)[3:]:
+#Join the Maxent results to it
+msg("Joining maxent results to catchment features")
+arcpy.AddJoin_management(catchLyr,"GRIDCODE",logTbl,"X")
+
+#Export the results
+msg("Copying results to {}".format(outFC))
+arcpy.Select_analysis(catchLyr, outFC,'"Logistic.X" > 0')
+
+#Remove all but the first two and last four fields
+msg("Cleaning up fields")
+for fld in arcpy.ListFields(outFC)[2:-4]:
     arcpy.DeleteField_management(outFC, fld.name)
 
-#Join the logistical output
-arcpy.JoinField_management(outFC,"GRIDCODE",logTbl,"X",logFld)
+#Create and update output fields
+msg("Adding habitat probability field...")
+arcpy.AddField_management(outFC,"HabProb","DOUBLE")
 
-#Add a new field for binary habitat
-arcpy.AddField_management(outFC,"Habitat","SHORT")
+msg("Updating habitat probability values...")
+arcpy.CalculateField_management(outFC,"HabProb","[Logistic_E]")
 
-#Select Rows above the threshold
-whereClause = '"{}" >= {}'.format(logFld[:10],threshold)
-habRecs = arcpy.MakeFeatureLayer_management(outFC,"selRecs",whereClause)
+msg("Adding habitat binary field...")
+arcpy.AddField_management(outFC,"HabBinary","Short")
 
-#Calculate the new field
-arcpy.CalculateField_management(habRecs,"Habitat",1)
+msg("Updating habitat binary values..")
+arcpy.CalculateField_management(outFC,"HabBinary","!Logistic_E! > {}".format(threshold),"PYTHON")
+
+#Remove fields
+msg("Cleaning up.")
+arcpy.DeleteField_management(outFC,["Logistic_O","Logistic_X","Logistic_Y","Logistic_E"])
+
