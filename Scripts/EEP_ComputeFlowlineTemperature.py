6d06c9ae9a014c78b6892f2b2fb555467e718508
johnpfay <john.fay@duke.edu>
2015-07-16 13:46:21 -0400

    Fixed issue occurring when all temp types don't occur

--- Scripts/EEP_ComputeFlowlineTemperature.py
+++ Scripts/EEP_ComputeFlowlineTemperature.py
@@ -38,9 +38,14 @@ def msg(txt,type="message"):
         arcpy.AddError(txt)
 
 # ---Procedures---
+## Select stream features within catchments
+msg("Subsetting stream features")
+thermalLyr  = arcpy.MakeFeatureLayer_management(thermalFC,"thermalLyr")
+arcpy.SelectLayerByLocation_management(thermalLyr,"HAVE_THEIR_CENTER_IN",catchmentsFC)
+
 ## Tag each stream habitat feature with the NHD Feature ID
 msg("Tagging thermal stream features with catchment IDs")
-arcpy.Intersect_analysis("{} #;{} # ".format(thermalFC,catchmentsFC), thermalFC_1,"NO_FID","","LINE")
+arcpy.Intersect_analysis("{} #;{} # ".format(thermalLyr,catchmentsFC), thermalFC_1,"NO_FID","","LINE")
 
 ## Summarize stats for each thermal class (labeled as the "HABITAT" field
 msg("Tabuluating stream temperature statistics for each catchment")
@@ -58,9 +63,15 @@ flds = []
 for f in arcpy.ListFields(flowlineTemperatureTbl):
     flds.append(f.name)
 #add the field if it doesn't exist
-if not "cold" in (flds): arcpy.AddField_management(flowlineTemperatureTbl,"cold","DOUBLE")
-if not "cool" in (flds): arcpy.AddField_management(flowlineTemperatureTbl,"cool","DOUBLE")
-if not "warm" in (flds): arcpy.AddField_management(flowlineTemperatureTbl,"warm","DOUBLE")
+if not "cold" in (flds):
+    arcpy.AddField_management(flowlineTemperatureTbl,"cold","DOUBLE")
+    arcpy.CalculateField_management(flowlineTemperatureTbl,"cold",0)
+if not "cool" in (flds):
+    arcpy.AddField_management(flowlineTemperatureTbl,"cool","DOUBLE")
+    arcpy.CalculateField_management(flowlineTemperatureTbl,"cool",0)
+if not "warm" in (flds):
+    arcpy.AddField_management(flowlineTemperatureTbl,"warm","DOUBLE")
+    arcpy.CalculateField_management(flowlineTemperatureTbl,"warm",0)
 
 ##Add total length field to table and calculate percentages
 msg("Converting percentages to absolute values")
