73ce6ebd84343412e2011875f49afec75ea9aad6
johnpfay <john.fay@duke.edu>
2015-08-11 13:12:53 -0400

    Set model inputs to work with ArcMap interface

--- Scripts/MAXENT_CalculateUplift.py
+++ Scripts/MAXENT_CalculateUplift.py
@@ -11,12 +11,20 @@ import arcpy, sys, os, csv
 arcpy.env.overwriteOutput = 1
 
 # Input variables
-sppName = "Nocomis_leptocephalus"
-resultFolder = r"C:\WorkSpace\EEP_Tool\HabitatStats\Nocomis_leptocephalus\Output_BU"
+sppName = arcpy.GetParameterAsText(0)
+currentFolder = arcpy.GetParameterAsText(1)
+resultFolder = arcpy.GetParameterAsText(2)
 scenario = resultFolder[-2:]
+respVarTbl = arcpy.GetParameterAsText(3)
+
+##sppName = "Nocomis_leptocephalus"
+##currentFolder = r"C:\WorkSpace\EEP_Tool\HabitatStats\Nocomis_leptocephalus\Output_HUC"
+##resultFolder = r"C:\WorkSpace\EEP_Tool\HabitatStats\Nocomis_leptocephalus\Output_BU"
+##scenario = resultFolder[-2:]
+##respVarTbl = r"C:\WorkSpace\EEP_Tool\Data\EEP_030501.gdb\ResponseVars"
 
 # Output
-upliftCSV = r"C:\WorkSpace\EEP_Tool\HabitatStats\Nocomis_leptocephalus_ME_BU.csv"
+upliftCSV = arcpy.GetParameterAsText(4)
 
 ## ---Functions---
 def msg(txt,type="message"):
@@ -29,50 +37,55 @@ def msg(txt,type="message"):
         arcpy.AddError(txt)
 
 ## ---Processes---
-#-- Get the MaxEnt result files---
-#ASCII projection file
+#-- Get the MaxEnt projections files---
+#Current conditions file
+curFile = os.path.join(currentFolder,"{}_HUC_prj.asc".format(sppName,scenario))
+if not os.path.exists(curFile):
+    msg("Cannot find current file: {}\nExiting.".format(ascFile),"error")
+    sys.exit(1)
+#Future conditions file
 ascFile = os.path.join(resultFolder,"{}_{}_prj.asc".format(sppName,scenario))
 if not os.path.exists(ascFile):
     msg("Cannot find projection file: {}\nExiting.".format(ascFile),"error")
     sys.exit(1)
-#Current prediction file
-sppFile = os.path.join(resultFolder,sppName + ".csv")
-if not os.path.exists(sppFile):
-    msg("Cannot find prediction file: {}\nExiting.".format(sppFile),"error")
-    sys.exit(1)
+msg("ASCII files found. Reading records")
 
-#Get the lines of the prediction file (likelihood under current conditions)
-f = open(sppFile,'r')
-sppLines = f.readlines()
+#--Get the lines of the current projection file (likelihood under current conditions)
+f = open(curFile, 'r')
+curLines = f.readlines()
 f.close()
-sppRecords = len(sppLines)
+curRecords = len(curLines)
+msg("  {} current records extracted".format(curRecords - 6))
 
-#Get the lines of the projection file (likelihood under alternate scenarios)
+#--Get the lines of the  future projection file (likelihood under alternate scenarios)
 f = open(ascFile, 'r')
 prjLines = f.readlines()
 f.close()
 prjRecords = len(prjLines)
+msg("  {} future records extracted".format(prjRecords - 6))
 
 #Initialize the output file
+msg("Initializing output CSV")
 f = open(upliftCSV,'w')
 f.write("GRIDCODE, {0}_current, {0}_alternate, {0}_uplift\n".format(scenario))
 
-#Loop through the records and write the output
-msg("Writing {} values to output file".format(sppRecords))
-for lineIdx in range(1,sppRecords):
-    sppLine = sppLines[lineIdx][:-1] #[:-1] is to skip newline char at end
-    prjLine = prjLines[lineIdx + 5][:-1]  #Skip ASCII header (6 lines); [:-1] is to skip newline char at end
-    #Get values from the spp line
-    sppLineData = sppLine[:-1].split(",")
-    gridCode = sppLineData[0][:-2] #First column is the catchment GRIDCODE; lop off decimal
-    curProb = float(sppLineData[-1]) #Last column is the logistic value
-    #Get values from the prj line; convert from scientific notation, if needed
-    prjProb = float(prjLine)
-    #Calculate uplift
-    uplift = prjProb - curProb
-    #Write outputs to file
-    f.write("%s,%2.5f,%2.5f,%2.5f\n" %(gridCode,curProb,prjProb,uplift))
+#Loop through catchment records in the response var table
+msg("Writing records to output CSV")
+recs = arcpy.da.SearchCursor(respVarTbl,"GRIDCODE")
+lineIdx = 6     #6 = offset to skip the ASCII header lines
+for rec in recs:
+    gridCode = rec[0]               #GRIDCODE of catchment
+    curProb = curLines[lineIdx]     #value from current projection ASCII
+    curVal = float(curProb)         # convert to number
+    prjLine = prjLines[lineIdx]     #value from future projection ASCII
+    prjVal = float(prjLine)         # convert to number
+    uplift = prjVal - curVal        # Uplift=difference between future and current
+    #write the record to the output CSV
+    f.write("%s,%2.5f,%2.5f,%2.5f\n" %(gridCode,curVal,prjVal,uplift))
+    #Increase lineIdx to read next line
+    lineIdx += 1
 
+msg("Closing file")
 f.close()
 
 
