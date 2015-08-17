547ffddf72fd531d73f798e3d02e5bed7f335b53
John Fay <John.Fay@duke.edu>
2015-08-17 09:43:01 -0400

    Script to write Maxent predictions as CSV files

--- Scripts/MAXENT_SavePredictions.py
+++ Scripts/MAXENT_SavePredictions.py
@@ -9,10 +9,10 @@ import arcpy, sys, os, csv
 arcpy.env.overwriteOutput = 1
 
 # Input variables
-maxentOutputFolder = arcpy.GetParameterAsText(0)
+maxentFolder = arcpy.GetParameterAsText(0)
 
 # Output
-outCSV = arcpy.GetParameterAsText(2) 
+outCSV = arcpy.GetParameterAsText(1) 
 
 # ---Functions---
 def msg(txt,type="message"):
@@ -44,10 +44,9 @@ for line in lines:
 msg("Getting Maxent files...")
 resultsFN = os.path.join(maxentFolder,"maxentResults.csv")
 logisticFN = os.path.join(maxentFolder,sppName+".csv")
-predictionFN = os.path.join(maxentFolder,sppName+"_samplePredictions.csv")
 
-#Make sure the results exist
-for f in (resultsFN, logisticFN, predictionFN):
+#Make sure the result files exist
+for f in (resultsFN, logisticFN):
     if not os.path.exists(f):
         msg("File {} does not exist.\nExiting.".format(f),"error")
         sys.exit()
@@ -62,9 +61,26 @@ idx = row1.index('Balance training omission, predicted area and threshold value 
 threshold = row2[idx]
 msg("Maxent logistic threshold set to {}".format(threshold))
 
+# Read in the predictions; make a list of values
+msg("reading in logistic values")
+predList = []
+f = open(logisticFN,'rt')
+row1 = f.readline()
+row2 = f.readline()
+while row2:
+    predList.append(row2.split(","))
+    row2 = f.readline()
+f.close()
+    
 # Write the output to a file
+msg("Creating Maxent Output File")
 f = open(outCSV,'w')
-outCSV.write("GRIDCODE, PROB, BIN")
-
-####GET THE SWD FILE, WRITE GRIDCODE, PROB, PRED, OBSERVED###
+f.write("GRIDCODE,PROB,HABITAT\n")
+for rec in predList:
+    gridcode = rec[0]
+    prob = float(rec[2])
+    if prob >= float(threshold): hab = 1
+    else: hab = 0
+    f.write("{},{},{}\n".format(gridcode,prob,hab))
+f.close()
 
